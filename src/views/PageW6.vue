dda46c5ad703136ca0b3084c3c508f6605e92ade
YenthMix <514013@vistacollege.nl>
2026-01-08 20:52:55 +0100

    gg

--- src/views/PageW6.vue
+++ src/views/PageW6.vue
@@ -12,9 +12,9 @@
                 <br />
                 <strong>Volledig:</strong> De kandidaat neemt actief deel aan het overleg waarbij relevante onderwerpen worden ingebracht en de juiste vragen worden gesteld.
             </p>
-          <Audio controls style="width: 100%; max-width: 500px; margin-top: 10px;">
-            <source src="/images/FeedBack/Klantengesprek.mp4" type="Audio/mp4" />
-        </Audio>
+          <audio controls style="width: 100%; max-width: 500px; margin-top: 10px;">
+            <source src="/images/FeedBack/Klantengesprek.mp4" type="Video/mp4" />
+        </audio>
         </div>
         <br />
 
